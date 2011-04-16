f00815d844f934a4872e8a55acc59441ac2d0456
Thomas Huston <twelveangrymen435@gmail.com>
2011-04-16 14:31:04 -0400

    added starter files

---
+++ malloclab-handout/mm.c
@@ -0,0 +1,111 @@
+/*
+ * mm-naive.c - The fastest, least memory-efficient malloc package.
+ * 
+ * In this naive approach, a block is allocated by simply incrementing
+ * the brk pointer.  A block is pure payload. There are no headers or
+ * footers.  Blocks are never coalesced or reused. Realloc is
+ * implemented directly using mm_malloc and mm_free.
+ *
+ * NOTE TO STUDENTS: Your code should begin with a header comment that
+ * describes the structure of your free and allocated blocks, the
+ * organization of the free list, and how your allocator manipulates
+ * the free list. each function should be preceeded by a header comment
+ * that describes what the function does.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "mm.h"
+#include "memlib.h"
+
+/*********************************************************
+ * NOTE TO STUDENTS: Before you do anything else, please
+ * provide your team information in the following struct.
+ ********************************************************/
+team_t team = {
+	/* Team name */
+	"tph",
+	/* First member's full name */
+	"Thomas Huston",
+	/* First member's NYU NetID*/
+	"tph227@nyu.edu",
+	/* Second member's full name (leave blank if none) */
+	"",
+	/* Second member's email address (leave blank if none) */
+	""
+};
+
+/* single word (4) or double word (8) alignment */
+#define ALIGNMENT 8
+
+/* rounds up to the nearest multiple of ALIGNMENT */
+#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+
+
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+/* 
+ * mm_init - initialize the malloc package.
+ */
+int mm_init(void) {
+	return 0;
+}
+
+/* 
+ * mm_malloc - Allocate a block by incrementing the brk pointer.
+ *     Always allocate a block whose size is a multiple of the alignment.
+ */
+void *mm_malloc(size_t size) {
+	int newsize = ALIGN(size + SIZE_T_SIZE);
+	void *p = mem_sbrk(newsize);
+	if (p == (void *)-1)
+		return NULL;
+	else {
+		*(size_t *)p = size;
+		return (void *)((char *)p + SIZE_T_SIZE);
+	}
+}
+
+/*
+ * mm_free - Freeing a block does nothing.
+ */
+void mm_free(void *ptr) {
+}
+
+/*
+ * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
+ */
+void *mm_realloc(void *ptr, size_t size) {
+	void *oldptr = ptr;
+	void *newptr;
+	size_t copySize;
+
+	newptr = mm_malloc(size);
+	if (newptr == NULL)
+		return NULL;
+	copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+	if (size < copySize)
+		copySize = size;
+	memcpy(newptr, oldptr, copySize);
+	mm_free(oldptr);
+	return newptr;
+}
+
+/*
+ * mm_check 
+ * It will check any invariants or consistency conditions you consider prudent.
+ * It returns a nonzero value if and only if your heap is consistent.
+ * You are encouraged to print out error messages when mm check fails.
+ * Is every block in the free list marked as free?
+ * Are there any contiguous free blocks that somehow escaped coalescing?
+ * Is every free block actually in the free list?
+ * Do the pointers in the free list point to valid free blocks?
+ * Do any allocated blocks overlap?
+ * Do the pointers in a heap block point to valid heap addresses?
+ */
+int mm_check(void) {
+	return 0;
+}
